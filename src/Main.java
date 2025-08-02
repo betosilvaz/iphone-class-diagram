65ffcbd4e124ce5ac12f851b28db52a5c2820c6a
betosilvaz <gilbertozn527@gmail.com>
2025-08-02 19:11:13 -0300

    using our entities

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import entities.IPhone;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        // Making use of our iPhone entity
+        IPhone phone = new IPhone();
+
+        // AparelhoTelefonico Interface
+        phone.ligar("558199999999");
+        phone.atender();
+        phone.iniciarCorreioDeVoz("558199999999");
+
+        // NavegadorDeInternet Interface
+        phone.exibirPagina("gilbertosilva.dev.br");
+        phone.adicionarNovaAba();
+        phone.atualizarPagina("gilbertosilva.dev.br");
+
+        //ReprodutorMusical Interface
+        phone.selecionarMusica("Beat It - Michael Jackson");
+        phone.pausar();
+        phone.tocar();
 
     }
 
